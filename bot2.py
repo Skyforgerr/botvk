c195ebadaccca4eb2801df489b45eb76256f54fa
cado-it <cado.it.prog@gmail.com>
2020-10-25 22:12:03 +0300

    12

--- bot2.py
+++ bot2.py
@@ -1,23 +1,50 @@
-import vk_api, vk
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from vk_api.utils import get_random_id
-
-vk_session = vk_api.VkApi(token='5681f33fa1a9e833372e21cae5c62cdddff34bbb0b18aca0f7ea069e9fc27a0ee08b94fe96d09305a8dcd')
-
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-longpoll = VkBotLongPoll(vk_session, club199739242)
-vk = vk_session.get_api()
+import time
+import vk_api
+import random
 from vk_api.longpoll import VkLongPoll, VkEventType
+#import u_photo
+ 
+ 
+def write_msg(rand_int, user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': rand_int})
+ 
+ 
+# API-ключ
+token = "87f971a64845627bca874a1b6ea25a9b89943201ebc0f7afb671d6c658945389d91b32c622e956611910b"
+ 
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+#vk = vk_api.VkApi(token = token, login = 'ЦЕНЗУРА', password = 'ЦЕНЗУРА')
+ 
+longpoll = VkLongPoll(vk)
+print("Бот запущен")
+ 
 
-for event in longpoll.listen():
-    if event.type == VkBotEventType.MESSAGE_NEW:
-    	if 'Ку' in str(event) or 'Привет' in str(event) or 'Хай' in str(event) or 'Хелло' in str(event) or 'Хеллоу' in str(event):
-    		if event.from_chat:
-    vk.messages.send(
-    key = (''),
-    server = (''),
-    ts=(''),
-    random_id = get_random_id(),
-    message='Привет!',
-    chat_id = event.chat_id
-    )
+# Основной цикл
+while True:
+    time.sleep(5)
+    for event in longpoll.listen():
+ 
+        # Если пришло новое сообщение
+        if event.type == VkEventType.MESSAGE_NEW:
+ 
+            if event.to_me:
+ 
+                request = event.text
+                randint = random.randint(100000000,900000000)
+                request = request.lower()
+                chat_id = vk.method('messages.getConversations')
+                chat_id = chat_id['items']
+                print(chat_id)
+ 
+                # Каменная логика ответа
+                for check in request:
+                   #id = vk.method('messages.getConversations')
+                   #id = id['items'][0]['last_message']['from_id']
+                   #print(id)'
+                    if request == "привет":
+                        write_msg(randint, event.user_id, "Хай")
+                    elif request == "пока":
+                        write_msg(randint, event.user_id, "Пока :(")
+                    else:
+                        write_msg(randint, event.user_id, "Не понял...")
