8b3d7632f162c8b9b9c20b5f2ef6cba0ea85fac3
Skyforgerr <70031110+Skyforgerr@users.noreply.github.com>
2020-10-25 22:09:21 +0300

    neeeeeeeeeeeeew

--- bot.py
+++ bot.py
@@ -1,16 +1,13 @@
-import vk_api, vk
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from vk_api.utils import get_random_id
+import vk_api
+from vk_api.bot_longpoll import VkLongPoll, VkEventType
+
 vk_session = vk_api.VkApi(token='5681f33fa1a9e833372e21cae5c62cdddff34bbb0b18aca0f7ea069e9fc27a0ee08b94fe96d09305a8dcd')
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-longpoll = VkBotLongPoll(vk_session, 'club199739242')
-vk = vk_session.get_api()
-from vk_api.longpoll import VkLongPoll, VkEventType
+session_api = vk_session.get_api()
+longpoll = VkLongPoll(vk_session)
 
 def sender(id, text): 
 	vk_session.method('messages.send', {'user_id': id, 'message' : text, 'random_id' : 0, 'chat_id': 1})
 
-	longpoll = VkLongPoll(vk_session, 'https://vk.me/join/A3EGVQmOfQ_QGwkqiNdagn0cky70reO1Q5A=')
 
 for event in longpoll.listen():
 	if event.type == VkEventType.MESSAGE_NEW:
